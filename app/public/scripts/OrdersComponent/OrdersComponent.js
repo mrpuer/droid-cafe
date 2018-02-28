756dac235ef0d9772a1458b5fb75605c706c1fdf
mrpuer-mini <vova@mytnik.net>
2018-02-28 17:07:12 +0300

    heroku db connection

--- app/public/scripts/OrdersComponent/OrdersComponent.js
+++ app/public/scripts/OrdersComponent/OrdersComponent.js
@@ -32,7 +32,7 @@ angular
         var minutes = Math.floor(millis / 60000);
         var seconds = ((millis % 60000) / 1000).toFixed(0);
         return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-      }
+      };
 
       mySocket.on('newOrder', function(){
         $ctrl.getUserOrders($ctrl.pagination);
@@ -59,6 +59,10 @@ angular
         }, function(err) {
           throw err;
         })
-      }
+      };
+
+      $ctrl.reorder = function(order) {
+
+      };
     }
   })
